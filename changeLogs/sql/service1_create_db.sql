ee1eba9a1faf0abb7d1c98fecf60d7770e91bda6
Debraj Manna <debraj.manna@nexla.com>
2025-08-06 20:11:40 +0530

    Update service1_create_db.sql

--- changeLogs/sql/service1_create_db.sql
+++ changeLogs/sql/service1_create_db.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
--- changeset service1:debraj.manna@nexla.com:JIRA:1
+-- changeset debraj.manna@nexla.com:JIRA_ID1-1
 -- comment: Create the service1_data table
 CREATE TABLE service1_data (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -9,7 +9,7 @@ CREATE TABLE service1_data (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- changeset service1:debraj.manna@nexla.com:JIRA:2
+-- changeset debraj.manna@nexla.com:JIRA_ID2-1
 -- comment: Add an index to the service_name column
 CREATE INDEX idx_service_name ON service1_data(service_name);
 -- rollback DROP INDEX idx_service_name ON service1_data;
